cd4bd5ac4f57abe99101de77ac6d431cb7b895c8
theSEAT <105782957+Thecoolcookie99@users.noreply.github.com>
2026-06-22 18:03:08 +1000

    w

---
+++ functions/api/auth.js
@@ -0,0 +1,21 @@
+export async function onRequest() {
+  const KEY_ID = B2_KEY_ID;
+  const APP_KEY = B2_APP_KEY;
+
+  const res = await fetch("https://api.backblazeb2.com/b2api/v2/b2_authorize_account", {
+    headers: {
+      Authorization: "Basic " + btoa(KEY_ID + ":" + APP_KEY)
+    }
+  });
+
+  const data = await res.json();
+
+  return new Response(JSON.stringify({
+    apiUrl: data.apiUrl,
+    authToken: data.authorizationToken,
+    bucketId: data.allowed.bucketId,
+    bucketName: data.allowed.bucketName
+  }), {
+    headers: { "Content-Type": "application/json" }
+  });
+}
